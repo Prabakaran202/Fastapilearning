a322feaf352384e522e0cabc43d8f7fcec829e25
prabakaran202 <prabakaran20020430@gmail.com>
2026-01-20 23:31:53 +0530

    week3 week2 revesion

---
+++ week3/app/schemas/inventory.py
@@ -0,0 +1,8 @@
+from pydantic import BaseModel,Field
+
+class Product(BaseModel):
+    id:int
+    name:str=Field(...,min_length=3)
+    price:float=Field(...,gt=0)
+    stock:int=Field(...,ge=0)
+    category:str
