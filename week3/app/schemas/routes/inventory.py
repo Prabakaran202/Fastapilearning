1f2d876bf9f341c73d69195c22deabe96464c0d9
prabakaran202 <prabakaran20020430@gmail.com>
2026-01-22 00:08:22 +0530

    new

--- week3/app/schemas/routes/inventory.py
+++ week3/app/schemas/routes/inventory.py
@@ -3,7 +3,7 @@ from schemas.inventory import Product,addPro
 from fastapi import APIRouter,HTTPException,Path,Query
 
 DB = [
-    {"id": 1, "name": "Laptop", "price": 50000, "stock": 5, "category": "Electronics"},
+    {"id": 1, "name": "Laptop", "price": 5000, "stock": 5, "category": "Electronics"},
     {"id": 2, "name": "Mouse", "price": 500, "stock": 0, "category": "Electronics"},
     {"id": 3, "name": "Monitor", "price": 8000, "stock": 2, "category": "Electronics"},
     {"id": 4, "name": "Keyboard", "price": 1200, "stock": 0, "category": "Electronics"}
@@ -17,7 +17,7 @@ router = APIRouter(prefix="/inventory",tags=["inventory"])
 async def getall():
     return DB
   
-@router.get("/inventory/search/")
+@router.get("/search/")
 async def minmax_item(min_price: int = Query(..., gt=0), 
     max_price: int = Query(..., le=10000)):
     
@@ -25,9 +25,22 @@ async def minmax_item(min_price: int = Query(..., gt=0),
 
     for item in DB:
 
-        if item["price"] <=min_price and item["price"] <=max_price:
+        if item["price"] >=min_price and item["price"] <=max_price:
             relt.append(item)
     if not relt:
         return{"mag":"your items not found"}
     
     return {"msg":"your items is","data":relt}
+
+@router.get("/out-of-stock/{outofstock}")
+async def stockof(item:int = 0):
+    relt=[]
+
+    for items in DB:
+        if items["stock"] == item : 
+            relt.append(items["name"])
+    if not relt:
+        return{"mag":"your items not found"}
+    
+    return {"msg":"your items is","data":relt}
+
