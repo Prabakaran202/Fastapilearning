8ac9ec2e9648a07282f19a99c4965456c5309677
prabakaran202 <prabakaran20020430@gmail.com>
2026-01-23 12:43:39 +0530

    test skill

---
+++ week3/test/main.py
@@ -0,0 +1,35 @@
+
+
+from fastapi import FastAPI,Path,Query
+from pydantic import BaseModel
+
+
+app=FastAPI()
+
+
+class bookstor(BaseModel):
+    name:str
+    aurthor:str
+    price:int
+
+
+db=[{"name":"nadaipayanam","aurthor":"praba","price":200},{"name":"unakenna","aurthor":"vetri","price":300},{"name":"nadipin kadhal","aurthor":"vadivu","price":200}]
+
+
+@app.post("/list/")
+
+def new(add:bookstor):
+    db.append(db.model_dump())  
+
+    return{"msg":"hello wait naa msg panarathu"}
+
+
+@app.get("/list/{id}")
+def find(id:str):
+    new=[]
+    for i in db:
+        if i["aurthor"] == id:
+            new.append(i["name"])
+            return{"msg":"youfind aurther book ","book":new}
+    return {"msg":"our auorthoe book not find "}    
+
