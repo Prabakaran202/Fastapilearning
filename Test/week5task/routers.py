bbe4bc67420ffad578e3ed49b5067d176cbcf60c
prabakaran202 <prabakaran20020430@gmail.com>
2026-01-27 00:00:13 +0530

    task complit

--- Test/week5task/routers.py
+++ Test/week5task/routers.py
@@ -1,2 +1,9 @@
 
+from fastapi import APIRouter
 
+from schemas import UserSchema
+
+router = APIRouter()
+@router.post("/users/", response_model=UserSchema)
+async def create_user(user: UserSchema):
+    return user
--- Test/week5task/schemas.py
+++ Test/week5task/schemas.py
@@ -1,2 +1,9 @@
 
+from pydantic import BaseModel, Field
 
+
+
+
+class UserSchema(BaseModel):
+     name: str = Field(..., title="name", max_length=40)
+     Class: str = Field(..., title="class", max_length=10)
