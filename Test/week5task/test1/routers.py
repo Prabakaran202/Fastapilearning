15a4a8df020f8a47c5762ad4c2e88bef5f2c4952
prabakaran202 <prabakaran20020430@gmail.com>
2026-01-30 23:30:35 +0530

    refactor: update user creation endpoint and schemas

--- Test/week5task/test1/routers.py
+++ Test/week5task/test1/routers.py
@@ -1,9 +1,16 @@
 
-from fastapi import APIRouter,Path, Query
+from fastapi import APIRouter,Path,Query,HTTPException
 
-from schemas import UserSchema
+
+
+from schemas import UserSchema,user
 
 router = APIRouter()
-@router.post("/users/{Mark}/{Name}")
-async def create_user(Mark: UserSchema =Path(..., title="Mark", ge=0, le=100),q:str=Query(None, title="Query", max_length=50),Name:UserSchema=Path(..., title="Name", max_length=40)):
-    return {"Mark": Mark, "Name": Name, "q": q}
+
+@router.post("/create_user/{name}/mark/{mark}", response_model=user)
+async def create_user(name: str = Path(..., title="The name of the user"), 
+                      mark: float = Path(..., title="The mark of the user"),query: str = Query(None, title="The query string")):
+    if mark < 0 or mark > 100:
+        raise HTTPException(status_code=400, detail="Mark must be between 0 and 100")
+    user_data = user(name=name, mark=mark, Class=query)
+    return user_data
--- Test/week5task/test1/schemas.py
+++ Test/week5task/test1/schemas.py
@@ -2,8 +2,14 @@
 from pydantic import BaseModel, Field
 
 
-
-
 class UserSchema(BaseModel):
      name: str = Field(..., title="name", max_length=40)
+     Class:str= Field(..., title="Class", max_length=10)
      mark: float = Field(..., title="mark", ge=0, le=100)
+
+
+class user(BaseModel):
+     name: str
+     mark: float
+     Class: str
+    
